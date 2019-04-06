08dead2b59d08d74c4aef115ef7157960c10c549
GMZN-Host <43391576+GMZN-Hostt@users.noreply.github.com>
2019-04-06 21:48:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require("fs");
-const pics = JSON.parse(fs.readFileSync('./pics.json' , 'utf8'));
 const ytdl = require("ytdl-core");
 const { Client, Util } = require('discord.js');
 const getYoutubeID = require('get-youtube-id');
